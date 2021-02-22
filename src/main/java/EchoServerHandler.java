95f3ca1afa4996b03f8917005296e9066864f593
张校捷 <zxjzxj9@gmail.com>
2021-02-23 00:09:02 +0800

    Add some test code

--- src/main/java/EchoServerHandler.java
+++ src/main/java/EchoServerHandler.java
@@ -1,6 +1,19 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.util.CharsetUtil;
 
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
-public class EchoServerHandler {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Sever received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
     public static void main(String[] args) {
 
     }
