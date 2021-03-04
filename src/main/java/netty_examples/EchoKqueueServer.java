2285e7ed325a77427d5f5155c68fdf1cf73d51f4
张校捷 <zxjzxj9@gmail.com>
2021-03-04 23:24:25 +0800

    Add kqueue based server code

--- src/main/java/netty_examples/EchoKqueueServer.java
+++ src/main/java/netty_examples/EchoKqueueServer.java
@@ -1,5 +1,15 @@
 package netty_examples;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.kqueue.KQueueEventLoopGroup;
+import io.netty.channel.kqueue.KQueueServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+
+import java.io.IOException;
+
 public class EchoKqueueServer {
 
     private final int port;
@@ -7,11 +17,30 @@ public class EchoKqueueServer {
         this.port = port;
     }
 
-    public void start() {
+    public void start() throws Exception {
+        final EchoServerHandler handler = new EchoServerHandler();
+        EventLoopGroup group = new KQueueEventLoopGroup();
 
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(KQueueServerSocketChannel.class)
+                    .localAddress(this.port)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception{
+                            ch.pipeline().addLast(handler);
+                        }
+                    });
+            ChannelFuture future= b.bind().sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            System.out.println("Shutting down the server...");
+            group.shutdownGracefully().sync();
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         EchoKqueueServer echoKqueueServer = new EchoKqueueServer(Integer.parseInt(args[0]));
         echoKqueueServer.start();
     }
