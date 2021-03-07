f860f9656b65175e05b325759ed3db10beb43cea
张校捷 <zxjzxj9@gmail.com>
2021-03-07 23:41:53 +0800

    Add some ByteBuf code

--- src/main/java/netty_examples/ByteBufEx.java
+++ src/main/java/netty_examples/ByteBufEx.java
@@ -1,6 +1,26 @@
 package netty_examples;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
+
 // Add examples of ByteBuf in Netty
 public class ByteBufEx {
 
+    public static void compositeBuffer(String s1, String s2) {
+        CompositeByteBuf buf = Unpooled.compositeBuffer();
+        ByteBuf header = Unpooled.copiedBuffer(s1.getBytes(StandardCharsets.UTF_8));
+        ByteBuf body = Unpooled.copiedBuffer(s2.getBytes(StandardCharsets.UTF_8));
+        buf.addComponents(header, body);
+        // System.out.println(buf.toString());
+        for(ByteBuf b: buf) {
+            System.out.println(b.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        compositeBuffer("Hello, ", "World!");
+    }
 }
