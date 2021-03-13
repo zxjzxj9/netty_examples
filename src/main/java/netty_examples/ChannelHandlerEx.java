5043b3aea3c6427a038e077c3bb4b324b1b2a31b
张校捷 <zxjzxj9@gmail.com>
2021-03-13 22:41:43 +0800

    Add ChannelHanderAdapter

--- src/main/java/netty_examples/ChannelHandlerEx.java
+++ src/main/java/netty_examples/ChannelHandlerEx.java
@@ -4,16 +4,22 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.ReferenceCountUtil;
 
 public class ChannelHandlerEx {
     @Sharable
     public class DiscardHandler extends ChannelInboundHandlerAdapter {
-
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) {
             ReferenceCountUtil.release(msg);
         }
+    }
 
+    @Sharable
+    public class SimpleDiscardHandler extends SimpleChannelInboundHandler<Object> {
+        @Override
+        public void channelRead0(ChannelHandlerContext ctx, Object msg) {
+        }
     }
 }
