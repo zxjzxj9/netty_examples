2612f4aa16ba49d264cfe309d1922e7bb77e15eb
张校捷 <zxjzxj9@gmail.com>
2021-03-16 22:20:03 +0800

    Add example to connect to ChannelHandler

--- src/main/java/netty_examples/ChannelHandlerEx.java
+++ src/main/java/netty_examples/ChannelHandlerEx.java
@@ -2,6 +2,7 @@ package netty_examples;
 
 import io.netty.channel.*;
 import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.concurrent.Promise;
 
@@ -15,7 +16,7 @@ public class ChannelHandlerEx {
     }
 
     @Sharable
-    public class SimpleDiscardHandler extends SimpleChannelInboundHandler<Object> {
+    public static class SimpleDiscardHandler extends SimpleChannelInboundHandler<Object> {
         @Override
         public void channelRead0(ChannelHandlerContext ctx, Object msg) {
             ReferenceCountUtil.release(msg);
@@ -23,13 +24,24 @@ public class ChannelHandlerEx {
     }
 
     @Sharable
-    public class DiscardChannelOutboundHandler extends ChannelOutboundHandlerAdapter {
-
+    public static class DiscardChannelOutboundHandler extends ChannelOutboundHandlerAdapter {
         @Override
         public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
             ReferenceCountUtil.release(msg);
             promise.setSuccess();
         }
+    }
+
+    public static void main(String args) {
+        ChannelHandler handler1 = new SimpleDiscardHandler();
+        ChannelHandler handler2 = new DiscardChannelOutboundHandler();
+        ChannelInitializer<SocketChannel> initializer = new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                socketChannel.pipeline().addLast(handler1);
+                socketChannel.pipeline().addLast(handler2);
+            }
+        };
     }
 
 }
