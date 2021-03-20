3a826a70622f72daebe498c77a034910c0a2370c
张校捷 <zxjzxj9@gmail.com>
2021-03-20 22:10:49 +0800

    Add more code to BootstrapServer example

--- src/main/java/netty_examples/BootstrapSever.java
+++ src/main/java/netty_examples/BootstrapSever.java
@@ -1,12 +1,42 @@
 package netty_examples;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
 
 public class BootstrapSever {
     void start() {
         NioEventLoopGroup group = new NioEventLoopGroup();
         ServerBootstrap bootstrap = new ServerBootstrap();
 
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new SimpleChannelInboundHandler<ByteBuf>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf buf) throws Exception {
+                        System.out.println("Receive data: " + buf.toString());
+                    }
+                });
+
+        ChannelFuture future = bootstrap.bind(new InetSocketAddress(8080));
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if(channelFuture.isSuccess()) {
+                    System.out.println("Channel complete success");
+                } else {
+                    System.out.println("Channel complete failed");
+                    channelFuture.cause().printStackTrace();
+                }
+            }
+        });
     }
 }
