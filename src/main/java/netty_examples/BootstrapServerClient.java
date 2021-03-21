069638f4f372de36f509409cad3fd4268a368d6a
张校捷 <zxjzxj9@gmail.com>
2021-03-21 22:10:45 +0800

    Add ServerClient bootstrap code

---
+++ src/main/java/netty_examples/BootstrapServerClient.java
@@ -0,0 +1,9 @@
+package netty_examples;
+
+// Server accept the connection, then launch a connection to another server
+// Client -> Server -> Server
+public class BootstrapServerClient {
+    void start() {
+
+    }
+}
