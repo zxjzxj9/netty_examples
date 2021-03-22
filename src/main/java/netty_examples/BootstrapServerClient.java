05d842bd0ffd391e34defd7af96df80443e962c4
张校捷 <zxjzxj9@gmail.com>
2021-03-22 23:12:11 +0800

    Add Server client to connect another server

--- src/main/java/netty_examples/BootstrapServerClient.java
+++ src/main/java/netty_examples/BootstrapServerClient.java
@@ -1,9 +1,57 @@
 package netty_examples;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
 // Server accept the connection, then launch a connection to another server
 // Client -> Server -> Server
 public class BootstrapServerClient {
     void start() {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new SimpleChannelInboundHandler<ByteBuf>() {
+                    ChannelFuture connectFuture;
+                    @Override
+                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                        Bootstrap bootstrap = new Bootstrap();
+                        bootstrap.channel(NioServerSocketChannel.class)
+                                .handler(new SimpleChannelInboundHandler<ByteBuf>() {
+                                    @Override
+                                    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf buf) throws Exception {
+                                        System.out.println("Data received: " + buf.toString());
+                                    }});
+                        bootstrap.group(ctx.channel().eventLoop());
+                        connectFuture = bootstrap.connect(
+                                new InetSocketAddress("www.manning.com", 80));
+                    }
 
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf buf) throws Exception {
+                        if(connectFuture.isDone()) {
+                            System.out.println("Finished connection...");
+                        }
+                    }
+
+                });
+
+        ChannelFuture future = bootstrap.bind(new InetSocketAddress(8080));
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if(channelFuture.isSuccess()) {
+                    System.out.println("Server bond...");
+                } else {
+                    channelFuture.cause().printStackTrace();
+                }
+            }
+        });
     }
 }
