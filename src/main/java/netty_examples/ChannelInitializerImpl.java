05b389664d9e31e89f05343627e26d87130fd9ac
张校捷 <zxjzxj9@gmail.com>
2021-03-23 22:22:39 +0800

    Add ChannelInitializerImpl code

---
+++ src/main/java/netty_examples/ChannelInitializerImpl.java
@@ -0,0 +1,16 @@
+package netty_examples;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+
+final public class ChannelInitializerImpl extends ChannelInitializer<Channel> {
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpClientCodec());
+        pipeline.addLast(new HttpObjectAggregator(Integer.MAX_VALUE));
+    }
+}
