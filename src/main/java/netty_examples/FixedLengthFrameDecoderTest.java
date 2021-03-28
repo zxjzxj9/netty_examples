aaa5f66c94258315c4d8b4c65e9aea34d2000da1
张校捷 <zxjzxj9@gmail.com>
2021-03-28 21:55:42 +0800

    Add another framedecoder test example

--- src/main/java/netty_examples/FixedLengthFrameDecoderTest.java
+++ src/main/java/netty_examples/FixedLengthFrameDecoderTest.java
@@ -32,7 +32,28 @@ public class FixedLengthFrameDecoderTest {
 
     @Test
     public void testFramesDecoded2() {
+        ByteBuf buf = Unpooled.buffer();
+        for(int i=0; i<9; i++) buf.writeByte(i);
+        ByteBuf input = buf.duplicate();
+        EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
 
+        assertFalse(channel.writeInbound(input.readBytes(2)));
+        assertTrue(channel.writeInbound(input.readBytes(7)));
+
+        assertTrue(channel.finish());
+        ByteBuf read = (ByteBuf) channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = (ByteBuf) channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = (ByteBuf) channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+        assertNull(channel.readInbound());
+        buf.release();
     }
 
 }
