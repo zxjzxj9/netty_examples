8b12025875b450b245834ac7d75441c6f0e69612
张校捷 <zxjzxj9@gmail.com>
2021-04-02 22:32:57 +0800

    Add message to message decoder

---
+++ src/main/java/netty_examples/IntegerToStringDecoder.java
@@ -0,0 +1,13 @@
+package netty_examples;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+public class IntegerToStringDecoder extends MessageToMessageEncoder<Integer> {
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, Integer integer, List<Object> list) throws Exception {
+        list.add(integer.toString());
+    }
+}
