8a69a5f3ecfe795a2725b3b9f476ffd850e8c6c3
张校捷 <zxjzxj9@gmail.com>
2021-04-03 22:28:06 +0800

    Add examples of too long frame exception

--- src/main/java/netty_examples/FixedLengthFrameDecoder.java
+++ src/main/java/netty_examples/FixedLengthFrameDecoder.java
@@ -3,11 +3,13 @@ package netty_examples;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.TooLongFrameException;
 
 import java.util.List;
 
 public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
     private final int frameLength;
+    private static final int MAX_FRAME_SIZE = 1024;
 
     public FixedLengthFrameDecoder(int frameLength) {
         if(frameLength < 0) throw new IllegalArgumentException(
@@ -17,6 +19,11 @@ public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> out) throws Exception {
+        int readable = byteBuf.readableBytes();
+        if (readable > MAX_FRAME_SIZE) {
+            byteBuf.skipBytes(readable);
+            throw new TooLongFrameException("Frame too big!");
+        }
         while(byteBuf.readableBytes() >= frameLength) {
             ByteBuf buf = byteBuf.readBytes(frameLength);
             out.add(buf);
