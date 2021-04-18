bede3f5b1a9d1c91ea8b6b38b6e51513c22ab1e0
张校捷 <zxjzxj9@gmail.com>
2021-04-18 21:59:23 +0800

    Add serializable code for implementation

---
+++ src/main/java/netty_examples/MarshallingInitializer.java
@@ -0,0 +1,36 @@
+package netty_examples;
+
+import io.netty.channel.*;
+import io.netty.handler.codec.marshalling.MarshallerProvider;
+import io.netty.handler.codec.marshalling.MarshallingDecoder;
+import io.netty.handler.codec.marshalling.MarshallingEncoder;
+import io.netty.handler.codec.marshalling.UnmarshallerProvider;
+
+import java.io.Serializable;
+
+public class MarshallingInitializer extends ChannelInitializer<Channel> {
+    private final MarshallerProvider marshallerProvider;
+    private final UnmarshallerProvider unmarshallerProvider;
+
+    public MarshallingInitializer(MarshallerProvider marshallerProvider, UnmarshallerProvider unmarshallerProvider) {
+        this.marshallerProvider = marshallerProvider;
+        this.unmarshallerProvider = unmarshallerProvider;
+    }
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+
+        pipeline.addLast(new MarshallingDecoder(unmarshallerProvider));
+        pipeline.addLast(new MarshallingEncoder(marshallerProvider));
+        pipeline.addLast(new ObjectHandler());
+    }
+
+    public final class ObjectHandler extends SimpleChannelInboundHandler<Serializable> {
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, Serializable serializable) throws Exception {
+
+        }
+    }
+}
