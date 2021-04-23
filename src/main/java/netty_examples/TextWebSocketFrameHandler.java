f684faa491325598e8c19dec5758ba3e90cd3787
张校捷 <zxjzxj9@gmail.com>
2021-04-23 22:17:35 +0800

    Add TextFrameHandler for WebSocket

---
+++ src/main/java/netty_examples/TextWebSocketFrameHandler.java
@@ -0,0 +1,33 @@
+package netty_examples;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx,
+                                   Object event) throws Exception {
+        if(event instanceof WebSocketServerProtocolHandler.HandshakeComplete){
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + " joined"));
+            group.add(ctx.channel());
+        } else {
+            super.userEventTriggered(ctx, event);
+        }
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame frame) throws Exception {
+        group.writeAndFlush(frame.retain());
+    }
+}
