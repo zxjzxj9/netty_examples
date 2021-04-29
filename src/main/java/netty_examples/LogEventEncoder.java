6535698b2d3783da7103c3e6eccfa5cddfcc5405
张校捷 <zxjzxj9@gmail.com>
2021-04-29 21:38:24 +0800

    Add LogEventEncoder to the DGRAM stream

---
+++ src/main/java/netty_examples/LogEventEncoder.java
@@ -0,0 +1,29 @@
+package netty_examples;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class LogEventEncoder extends MessageToMessageEncoder<LogEvent> {
+    private final InetSocketAddress remoteAddress;
+
+    public LogEventEncoder(InetSocketAddress remoteAddress) {
+        this.remoteAddress = remoteAddress;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, LogEvent event, List<Object> list) throws Exception {
+        byte[] file = event.getLogfile().getBytes(CharsetUtil.UTF_8);
+        byte[] msg = event.getMsg().getBytes(CharsetUtil.UTF_8);
+        ByteBuf buf = ctx.alloc().buffer(file.length + msg.length + 1);
+        buf.writeBytes(file);
+        buf.writeByte(LogEvent.SEPARATOR);
+        buf.writeBytes(msg);
+        list.add(new DatagramPacket(buf, remoteAddress));
+    }
+}
