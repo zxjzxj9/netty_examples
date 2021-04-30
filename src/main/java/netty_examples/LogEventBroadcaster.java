3eaa0fb4fcd4cfed10da4aaaa5b44d3ec738af1d
张校捷 <zxjzxj9@gmail.com>
2021-04-30 22:01:02 +0800

    Add example of broadcast server

---
+++ src/main/java/netty_examples/LogEventBroadcaster.java
@@ -0,0 +1,80 @@
+package netty_examples;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+
+public class LogEventBroadcaster {
+    private final EventLoopGroup group;
+    private final Bootstrap bootstrap;
+    private final File file;
+
+    public LogEventBroadcaster(InetSocketAddress address, File file) {
+        group = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(group).channel(NioDatagramChannel.class)
+                .option(ChannelOption.SO_BROADCAST, true)
+                .handler(new LogEventEncoder(address));
+        this.file = file;
+    }
+
+    public void run() throws Exception {
+        Channel ch = bootstrap.bind(0).sync().channel();
+        long pointer = 0;
+
+        for(;;) {
+            long len = file.length();
+            if (len < pointer) {
+                // file was reset
+                pointer = len;
+            } else if (len > pointer) {
+                RandomAccessFile raf = new RandomAccessFile(file, "r");
+                raf.seek(pointer);
+                String line;
+
+                while ((line = raf.readLine()) != null) {
+                    ch.writeAndFlush(new LogEvent(null,
+                            file.getAbsolutePath(), line, -1));
+                    pointer = raf.getFilePointer();
+                    raf.close();
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                    break;
+                }
+
+            }
+        }
+    }
+
+    public void stop() {
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            throw new IllegalArgumentException();
+        }
+
+        LogEventBroadcaster broadcaster = new LogEventBroadcaster(
+                new InetSocketAddress("255.255.255.255",
+                        Integer.parseInt(args[0])), new File(args[1]));
+        broadcaster.run();
+        try {
+        }
+        finally {
+            broadcaster.stop();
+        }
+    }
+
+}
